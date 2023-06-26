d140cc65f5c0d43238cd08495a30a7fd8d55705b
mayank_git <mayankmali45@gmail.com>
2023-06-26 21:39:24 +0530

    job appw with frontend

---
+++ routes/jobs.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const router = express.Router()
+const testUser = require('../middleware/testUser')
+
+const {
+  createJob,
+  deleteJob,
+  getAllJobs,
+  updateJob,
+  getJob,
+  showStats
+} = require('../controllers/jobs')
+
+router.route('/')
+  .post(testUser, createJob)
+  .get(getAllJobs)
+
+router.route('/stats').get(showStats)
+
+router.route('/:id')
+  .get(testUser, getJob)
+  .delete(testUser, deleteJob)
+  .patch(testUser, updateJob)
+
+module.exports = router
